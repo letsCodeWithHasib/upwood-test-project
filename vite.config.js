4254c8752e99ad2d8a59fb07ed799e77c9a723e4
letsCodeWithHasib <mohdhasib2001@gmail.com>
2024-10-07 17:00:57 +0530

    please chal ja bhai

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,19 @@ import { nodePolyfills } from "vite-plugin-node-polyfills";
 
 export default defineConfig({
   build: {
-    chunkSizeWarningLimit: 1000, // Increase the limit to 1000 kB
+    emptyOutDir: true, // Ensure the output directory is cleared before each build
+    chunkSizeWarningLimit: 5000, // Set the chunk size warning limit to 1000 kB
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          // Separate node_modules into a vendor chunk
+          if (id.includes("node_modules")) {
+            return "vendor";
+          }
+          // Additional manual chunking logic can be added here
+        },
+      },
+    },
   },
   plugins: [react(), nodePolyfills()],
 });
