6b53ab664ca8fed32dc98cb1ea5feac3fee14469
letsCodeWithHasib <mohdhasib2001@gmail.com>
2024-10-07 16:41:16 +0530

    all good

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,8 @@ import react from "@vitejs/plugin-react";
 import { nodePolyfills } from "vite-plugin-node-polyfills";
 
 export default defineConfig({
+  build: {
+    chunkSizeWarningLimit: 1000, // Increase the limit to 1000 kB
+  },
   plugins: [react(), nodePolyfills()],
 });
